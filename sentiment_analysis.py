fd0abaa6600c68ecf3796367e19f1aae6b1b8ea9
liuchenyang <liuchenyang1204@gmail.com>
2021-12-10 16:46:05 +0800

    优化代码

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,19 +6,18 @@ from mongo import connect_to_mongo
 comments = 'comments'
 collection = connect_to_mongo(comments)
 
+def get_comment_text():
+    results = list(collection.find({},{'_id':0,'comment_text':1}))
+    result= [result[ 'comment_text' ] for result in results]
+    return result
 
-result = pd.DataFrame(list(collection.find()))
+results = get_comment_text()
+score=[]
 
-result.to_csv('comments.csv', encoding='utf_8_sig')
-
-df = pd.read_csv("comments.csv")
-result = list(df["comment_text"])
-score = []
-
-for i in range(len(result)):
+for i in range(len(results)):
     try:
-        blob = SnowNLP(result[i])
-
+        blob = SnowNLP(results[i])
+        print(blob.sentiments)
         score.append(blob.sentiments)
     except Exception:
         pass
